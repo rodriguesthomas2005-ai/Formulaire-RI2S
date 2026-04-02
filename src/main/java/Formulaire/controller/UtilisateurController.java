d9f2b3d0b46159e1eea532aea13786fc0c1e430f
rodriguesthomas2005-ai <thomas.rodrigues_da_conceicao@etud.univ-jfc.fr>
2026-04-02 22:07:11 +0200

    02/04 modif vérification utilisateur

--- src/main/java/Formulaire/controller/UtilisateurController.java
+++ src/main/java/Formulaire/controller/UtilisateurController.java
@@ -66,10 +66,10 @@ public class UtilisateurController {
                                       @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date dateNaissance) {
         Optional<Utilisateur> user = utilisateurService.verifierExistence(nom, prenom, dateNaissance);
         if (user.isPresent()){
-            if (typeutilisateur =="profilpro" && user.get().getProfilPro() == null) {
+            if ("profilpro".equals(typeutilisateur) && user.get().getProfilPro() == null) {
                 return ResponseEntity.ok(Map.of("existe", false, "id", user.get().getIdUtilisateur()));
             }
-            else if (typeutilisateur =="profilnonpro" && user.get().getProfilNonPro() == null) {
+            else if ("profilnonpro".equals(typeutilisateur) && user.get().getProfilNonPro() == null) {
                 return ResponseEntity.ok(Map.of("existe", false, "id", user.get().getIdUtilisateur()));
             }
             return ResponseEntity.ok(Map.of("existe", true, "id", user.get().getIdUtilisateur()));
